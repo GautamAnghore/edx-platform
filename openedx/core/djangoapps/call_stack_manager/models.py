fb8c960a39652719d96aca963272f60e00391905
utkjad <utkarsh@edx.org>
2015-06-16 14:25:00 +0000

    changes in call_stack_manager/models.py

--- openedx/core/djangoapps/call_stack_manager/models.py
+++ openedx/core/djangoapps/call_stack_manager/models.py
@@ -1,6 +1,8 @@
 """
-Dummy models file
+Dummy models.py file
 
 Note -
-Django tests searches for models.py for creating test models.
+django-nose loads models for tests, but only if the django app that the test is contained in has models itself.
+This file is empty so that the unit tests can have models.
+For call_stack_manager - models specific to tests are defined in tests.py
 """
